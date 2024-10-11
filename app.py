70b95f44d21d98d427ffb22ce7dafe86a1e6f7f5
JooYpung Kim <ilu52046224@gmail.com>
2024-10-11 11:00:54 +0900

    Configure port for Render

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-# app.py
-
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# 메인 페이지 렌더링
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Render는 환경 변수로 포트를 설정함
+    app.run(host='0.0.0.0', port=port)
