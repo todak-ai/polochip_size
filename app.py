818bd075a540f1d21b7b28279ba8a120bf0563ae
JooYpung Kim <ilu52046224@gmail.com>
2024-11-23 22:59:25 +0900

    Update versions

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
 from flask import Flask, render_template
+from flask_caching import Cache
 
 app = Flask(__name__, static_folder='static')
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 @app.route('/')
 def index():
@@ -9,6 +11,5 @@ def index():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # Render는 환경 변수로 포트를 설정함
-    app.run(host='0.0.0.0', port=port)
-
+    app.run(host='0.0.0.0', port=5000, debug=False)  # debug=False 설정
 
