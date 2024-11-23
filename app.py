6ae2a4ee908a0552501ec3c9f406932bb5feb5f5
JooYpung Kim <ilu52046224@gmail.com>
2024-11-23 23:10:42 +0900

    Update versions

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ def index():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # Render는 환경 변수로 포트를 설정함
-    app.run(host='0.0.0.0', port=5000, debug=False)  # debug=False 설정
+    app.run(host='0.0.0.0', port=port, debug=False)  # debug=False 설정
 
